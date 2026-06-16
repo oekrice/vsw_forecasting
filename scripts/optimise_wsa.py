e333da2ef7c8ada965067f36e4ee8c84bbd13e1e
Oliver on Maths Linux <vgjn10@airy.durham.ac.uk>
2026-06-16 10:33:09 +0100

    Added argv for optimise run

--- scripts/optimise_wsa.py
+++ scripts/optimise_wsa.py
@@ -30,27 +30,55 @@ nsamples = 50
 #Will need a lookup table or equivalent to find data which matches things as they should.
 #Can specify file name to look up WSA parameters? Yeah, probably.
 
+run_names = ["p2g", "p5g", "o2g", "o5g", "p2h", "p5h", "o2h", "o5h"]
+
+if len(sys.argv) > 1:
+    batch_id = int(sys.argv[1])
+else:
+    raise Exception('Specify batch number.')
+
+#Get the model setup depending on the batch numbers
+if (batch_id//2)%2 == 0:
+    is_pfss = True
+    model = "pfss"
+else:
+    is_pfss = False
+    model = "outflow"
+
+
+if (batch_id%2) == 0:
+    rss = 2.5
+else:
+    rss = 5.0
+
+if (batch_id//4) == 0:
+    source = "gong"
+else:
+    source = "hmi"
+
+run_name = run_names[batch_id]
+
+
 test_parameters = {"observation_time": obs_times,
-                  "base_name": "p2g",
-                  "run_name": "optimise_run_2",
-                  "model_type": "pfss",
-                  "calculate_base_model": False,
-                  "overwrite_base_model": False,
-                  "calculate_huxt": True,
-                  "r_ss": 2.5,
-                  "WSA_type": "standard",
-                  "WSA_parameters": None,
-                  "verbose": True,
-                  "data_source": "gong",
-                  "resolutions": [120,180,360],
-                  "r_hb": 21.5,
-                  "match_flag": True,
-                  "velocity_type": "wsa",
-                  "spinup_time": 5,
-                  "forecast_length": 5,
-                  "verbose": False,
-                  "optimisation_type": "distribution",
-                  "do_plots": False}
+                "base_name": run_names[batch_id],
+                "run_name": f"optimise_run_{batch_id}",
+                "model_type": model,
+                "calculate_base_model": False,
+                "overwrite_base_model": False,
+                "calculate_huxt": False,
+                "r_ss": rss,
+                "WSA_type": "standard",
+                "WSA_parameters": None,
+                "data_source": source,
+                "resolutions": [120,180,360],
+                "r_hb": 21.5,
+                "match_flag": False,
+                "velocity_type": "wsa",
+                "spinup_time": 5,
+                "forecast_length": 5,
+                "verbose": False,
+                "optimisation_type": "distribution",
+                "do_plots": False}
 
 def evaluate_with_timeout(pool, theta, timeout=60):
     result = pool.apply_async(evaluate_theta_safe, (theta,))
